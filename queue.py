b147b67a49fd3c761323c89b1313e0a7cfcb52ca
Landriou <39389586+Landriou@users.noreply.github.com>
2023-05-16 00:01:41 -0300

    Add files via upload

---
+++ queue.py
@@ -0,0 +1,13 @@
+from linkedlist import *
+
+
+
+def enqueue(Q, element):
+    add(Q,element)
+
+def dequeue(Q):
+    if length(Q) == 0:
+        return None
+    element = access(Q,length(Q)-1)
+    deleteposition(Q, length(Q)-1)
+    return element
